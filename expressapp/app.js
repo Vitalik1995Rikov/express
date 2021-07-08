798d301eb4a86fb486ced641e3fc3f016fbb3579
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-08 16:25:29 +0300

    express start

---
+++ expressapp/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(request, response) {
+    response.send("<h2>Привет Express<h2>");
+});
+
+app.get("/about", function(request, response) {
+    response.send("<h1>О сайте<h1>");
+});
+
+app.get("/contact", function(request, response) {
+    response.send("<h1>Контакты</h1>");
+});
+
+app.listen(3000);
